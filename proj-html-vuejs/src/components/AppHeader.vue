4749b25ade8480d9fff7633f3318a32e7ca11140
FraBif <francescobif@hotmail.it>
2023-06-27 03:43:54 +0200

    Updated header and created navbar component

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -1,11 +1,79 @@
 <template>
- 
+  <header>
+    <section class="my_first_section">
+        
+          <img src="../assets/desktop version img/logo.png" alt="logo">
+          <HeaderNav/>  
+          <div class="sidebar">
+            <span>
+                <i class="fa-solid fa-headphones fa-xl" style="color: #ffffff;"></i>
+                1.800.123.4567
+            </span>
+          </div>
+          <div class="button-1">
+             Get a free consultation
+          </div>
+
+        
+        
+    </section>
+  </header>
 </template>
 
 <script>
-
+  import HeaderNav from './HeaderNav.vue';
+  export default {
+        components: {
+            HeaderNav,
+        },
+        data() {
+            return {
+                
+            }
+        },
+        created() {
+            
+        },
+        mounted() {
+            
+        },
+        props: {
+        },
+        computed: {
+            
+        },
+        methods: {
+            
+        },
+    }
 </script>
 
 <style scoped>
+ header{
+    background-color: #0d1267;
+    width: 100%;
+    padding: 3rem;
+ }
 
+ .my_first_section{
+    display: flex;
+    align-items: center;
+ }
+
+ .sidebar{
+    color: white;
+    font-size: 1.3rem;
+    margin-left: 370px;
+ }
+
+ .button-1 {
+  background-color:white; 
+  color: #000545;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-left: 100px;
+}
 </style>
---
+++ proj-html-vuejs/src/components/HeaderNav.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav>
+    <ul>
+        <li v-for="link in links">
+            {{ link.text }}
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+      export default {
+        components: {
+        },
+        data() {
+            return {
+                links: [
+                    {
+                        text: "Home",
+                        href: "#"
+                    },
+                    {
+                        text: "About",
+                        href: "#"
+                    },
+                    {
+                        text: "Services",
+                        href: "#"
+                    },
+                    {
+                        text: "Journal",
+                        href: "#"
+                    },
+                    {
+                        text: "Contact",
+                        href: "#"
+                    },
+                ]
+            }
+        },
+        created() {
+            
+        },
+        mounted() {
+            
+        },
+        props: {
+        },
+        computed: {
+            
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style scoped>
+li{
+    color: white;
+    list-style: none;
+    margin-left: 2rem;
+    font-size: 1.5rem;
+}
+
+ul{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
